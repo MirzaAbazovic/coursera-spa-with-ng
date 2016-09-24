42e0417cc3d4f49b68b9b88dc520919866239416
mirza.abazovic <mirza.abazovic@gmail.com>
2016-09-24 23:00:38 +0200

    solution tu push

--- assignments/assignment2/js/app.js
+++ assignments/assignment2/js/app.js
@@ -3,31 +3,27 @@
     angular.module('ShoppingListCheckOff',[])
     .controller('ToBuyShoppingController',ToBuyShoppingController)
     .controller('AlreadyBoughtShoppingController',AlreadyBoughtShoppingController)
-    .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
+    .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
     ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyShoppingController(shoppingListCheckOffService){
         var ctrl = this;
         ctrl.itemsToBuy = shoppingListCheckOffService.itemsToBuy;
-        ctrl.bought = shoppingListCheckOffService.buyItem;
+        ctrl.buyItem = shoppingListCheckOffService.buyItem;
     };
 
-    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService']
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtShoppingController(shoppingListCheckOffService){
         var ctrl = this;
         ctrl.boughtItems = shoppingListCheckOffService.boughtItems;
-
     };
 
     function ShoppingListCheckOffService(){
         var service = this;
-        service.itemsToBuy = [{ name: "cookies", quantity: 10 },{ name: "milk", quantity: 1 },{ name: "eggs", quantity: 12 },{ name: "yougurt", quantity: 2 }];
+        service.itemsToBuy = [{ name: "cookies", quantity: 10 },{ name: "milk", quantity: 1 },{ name: "eggs", quantity: 12 },{ name: "yougurts", quantity: 2 }];
         service.boughtItems = [];
         service.buyItem = function(itemIndex){
-            var boughtItem = service.itemsToBuy.splice(itemIndex,1);
-            service.boughtItems.push(boughtItem[0]);
-
+            service.boughtItems.push(service.itemsToBuy.splice(itemIndex,1)[0]);
         };
-
     };
 })()
